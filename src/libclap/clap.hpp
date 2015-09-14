d2ab16ac4efc3b2123c5d5e5078348d3a793f8a0
Stephan Lorenzen <stephan.lorenzen@gmail.com>
2015-09-14 22:43:32 +0200

    Added command line argument parser library.

---
+++ src/libclap/clap.hpp
@@ -0,0 +1,267 @@
+#ifndef CLAP_CLAP_H
+#define CLAP_CLAP_H
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+/**
+ * @class CLAP
+ * @author Stephan Lorenzen
+ *
+ * The [C]ommand [L]ine [A]rgument [P]arser, used for easily configuring a program
+ * for parsing command line arguments.
+ */
+class CLAP {
+public:
+  /**
+   * CLAP Constructor
+   * Constructs the CLAP from a configuration string and the input arguments.
+   * The configuration string must have the following format: 
+   *
+   *       +--------------------------------------------------------+
+   *       | OPTIONS:                                               |
+   *       | -o1 --option1 [br]  [var1:(i|f|b|s)] [var2: ...] ...   |
+   *       |  .                                                     |
+   *       |  .                                                     |
+   *       |  .                                                     |
+   *       | PARAMETERS:                                            |
+   *       | par1:(i|f|b|s) [par2:(i|f|b|s)] ...                    |
+   *       |  .                                                     |
+   *       |  .                                                     |
+   *       |  .                                                     |
+   *       +--------------------------------------------------------+
+   *
+   * Options are defined in the OPTIONS section.
+   * 'o1' is the short name for an option, 'option1' is the long name for that same option.
+   * 'br' is optional - if set, the parser will stop without error when encountering this option.
+   * 'var1', 'var2', ... are the (optional) parameters for the option. The type follows the ':'
+   * and must be either 'i' = integer, 'f' = float, 'b' = boolean, 's' = string.
+   * 
+   * Mandatory parameters are defined in the PARAMETERS section. Here, several accepted patterns
+   * can be defined. Leave an empty line (or no lines) for no parameters.
+   * 'par1' is the name of the first parameter. Again, the type is specified after the ':'.
+   *
+   * Both the OPTIONS and the PARAMETERS sections may be left empty.
+   *
+   * @param info The configuration string. This string must have the format given above.
+   * @param argc Size of argv array.
+   * @param argv Array containing command line arguments.
+   */
+  CLAP(const std::string info, unsigned int argc, char **argv);
+  
+  /**
+   * Destructor
+   * Cleans up the CLAP structure.
+   */
+  ~CLAP();
+
+  /**
+   * Checks if an option/parameter is set.
+   *
+   * @param name The name of the option/parameter
+   * @return integer The number of times the parameter has been passed
+   */
+  unsigned int is_set(const std::string name);
+
+  /**
+   * Gets a boolean parameter of some option.
+   *
+   * @param name The name of the option
+   * @param n The n'th parameter of the option
+   * @param m The m'th occurence of the option
+   * @return bool The value given
+   */
+  bool get_bool_param(const std::string name, unsigned int n=0, unsigned int m=0);
+  
+  /**
+   * Gets an integer parameter of some option.
+   *
+   * @param name The name of the option
+   * @param n The n'th parameter of the option
+   * @param m The m'th occurence of the option
+   * @return int The value given
+   */
+  int get_int_param(const std::string name, unsigned int n=0, unsigned int m=0);
+  
+  /**
+   * Gets a floating point parameter of some option.
+   *
+   * @param name The name of the option
+   * @param n The n'th parameter of the option
+   * @param m The m'th occurence of the option
+   * @return float The value given
+   */
+  float get_float_param(const std::string name, unsigned int n=0, unsigned int m=0);
+  
+  /**
+   * Gets a string parameter of some option.
+   *
+   * @param name The name of the option
+   * @param n The n'th parameter of the option
+   * @param m The m'th occurence of the option
+   * @return string The value given
+   */
+  std::string get_string_param(const std::string name, unsigned int n=0, unsigned int m=0);
+
+  /**
+   * Get the chosen pattern. If a break has occured, -1 is returned.
+   *
+   * @return int The chosen pattern
+   */
+  int get_chosen_pattern();
+
+  /**
+   * Prints a usage error - used when an input is somehow invalid.
+   *
+   * @param errMsg The errMsg to show the user.
+   */
+  void error_usage(std::string errMsg);
+
+  /**
+   * Prints the help text.
+   */
+  void print_help();
+protected:
+private:
+  /**
+   * Parameter types.
+   */
+  enum Type {
+    bool_t,
+    int_t,
+    float_t,
+    string_t
+  };
+  
+  /**
+   * Checks if a string is an option
+   * 
+   * @param arg The argument to be cleaned up.
+   * @return True if '-' or '--' removed, otherwise false.
+   */
+  bool is_option(std::string &arg);
+
+  /**
+   * Returns a void pointer to the value of the parameter.
+   *
+   * @param fname Function name to print in error message on error.
+   * @param t Expected type of value.
+   * @param pname Name of the option
+   * @param m The m'th occurence of the option 
+   * @param n The n'th parameter of the option
+   * @return void* Pointer to the value of this parameter
+   */
+  void *_get_param(const std::string fname, CLAP::Type t,
+		   const std::string pname, unsigned int n,
+		   unsigned int m);
+
+  /**
+   * Parses an integer from a string.
+   *
+   * @param arg The input string
+   * @param val Reference, where the result is stored
+   * @return True if arg contains an integer, otherwise false
+   */
+  bool parse_int(std::string &arg, int &val);
+
+  /**
+   * Parses a boolean from a string.
+   *
+   * @param arg The input string
+   * @param val Reference, where the result is stored
+   * @return True if arg contains a boolean, otherwise false
+   */
+  bool parse_bool(std::string &arg, bool &val);
+
+  /**
+   * Parses a float from a string.
+   *
+   * @param arg The input string
+   * @param val Reference, where the result is stored
+   * @return True if arg contains a float, otherwise false
+   */
+  bool parse_float(std::string &arg, float &val);
+
+  /**
+   * @struct CLAP::Param
+   *
+   * Models parameters in CLAP
+   */
+  struct Param {
+    /**
+     * Constructor for CLAP::Param. Constructs the parameter from a
+     * string formatted as follows: 'name:(i|f|b|s)', where 'name'
+     * is the name of the parameter and one of the types 'i' = integer,
+     * 'f' = float, 'b' = boolean, 's' = string, must be chosen.
+     *
+     * @param info The input string, formatted as shown above.
+     */
+    Param(const std::string info);
+    
+    /**
+     * Destructor for CLAP::Param
+     * Cleans up the Param structure.
+     */
+    ~Param();
+    
+    /** Name of the parameter */
+    std::string name;
+    /** Type of the parameter */
+    Type  t;
+  };
+
+  /**
+   * @struct CLAP::Option
+   *
+   * Models options in CLAP
+   */
+  struct Option {
+    /**
+     * Constructor for CLAP::Option. Constructs the option from a
+     * string formatted as follows:
+     *  '-o -option [br] [var1:(i|f|b|s)] [var2:...]...', where 'o'/'option'
+     * is the short/full name of the option, 'br' is optional and will set the
+     * option to stop the parser if it occurs, and 'var1', 'var2', ... are the
+     * parameters, with types 'i' = integer, 'f' = float, 'b' = boolean or
+     * 's' = string.
+     *
+     * @param info The input string, formatted as shown above.
+     */
+    Option(const std::string info);
+    
+    /**
+     * Destructor for CLAP::Option
+     * Cleans up the Option structure.
+     */
+    ~Option();
+    
+    /** Option name */
+    std::string name;
+    /** Option short name */
+    std::string short_name;
+    /** Integer indicating number of times set */
+    unsigned int is_set;
+    /** Flag indicating whether the option should stop the parser */
+    bool do_break;
+    /** List of parameters of this option */
+    std::vector<Param> params;
+    /** List of arguments */
+    std::vector<void*> args;
+  };
+
+  /** Name of the executable */
+  std::string exec_name;
+  /** List of the available options for this program */
+  std::vector<Option> options;
+  /** Map of the options for this program */
+  std::unordered_map<std::string, unsigned int> map;
+  /** Selected pattern */
+  int sel_pattern;
+  /** List of mandatory parameter patterns for this program */
+  std::vector< std::vector<Param> > patterns;
+  /** List of arguments */
+  std::vector<void*> args;
+};
+
+#endif // CLAP_CLAP_H
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <stdlib.h>
 
+#include "libclap/clap.hpp"
 #include "test/test.hpp"
 #include "steiner/utils/delaunay.hpp"
 #include "steiner/utils/utils.hpp"
@@ -25,17 +26,52 @@ typedef Utils::Point      Point;
 typedef Utils::Edge       Edge;
 typedef Utils::Generator  Generator;
 
-void usage();
-std::string getString(std::string tag, int argc, char *argv[], bool mandatory=false);
-bool getBool(std::string tag, int argc, char *argv[]);
-int getInt(std::string tag, int argc, char *argv[], int def=-1, bool mandatory=false);
-std::vector<Point> getSet(int argc, char *argv[]);
-
+int set_type_from_name(std::string &settype) {
+  int t = -1;
+  if(settype.compare(0, 20, "random") == 0)
+    t = TEST_POINT_SET_RANDOM_D;
+  else if(settype.compare(0, 20, "simplex") == 0)
+    t = TEST_POINT_SET_SIMPLEX;
+  else if(settype.compare(0, 20, "sausage") == 0)
+    t = TEST_POINT_SET_SAUSAGE;
+  else if(settype.compare(0, 20, "grid") == 0)
+    t = TEST_POINT_SET_GRID;
+  else if(settype.compare(0, 20, "delaunay") == 0)
+    t = TEST_POINT_SET_DELAUNAY_SIMPLICES;
+  return t;
+}
 
 int main(int argc, char *argv[]) {
-  if(argc < 2)
-    usage();
-  if (std::string("testtopo").compare(0, 20, argv[1]) == 0) {
+  const std::string clap_config =
+    "OPTIONS:                                                              \n"
+    "-os   --onlysgh                                                       \n"
+    "-ct   --counttest  br dimension:i num_points:i seed:i                 \n"
+    "-tt   --testtopo   br                                                 \n"
+    "-v    --verbose                                                       \n"
+    "-npo  --nopostopt                                                     \n"
+    "-nsc  --nosubcon                                                      \n"
+    "-rdc  --redocon                                                       \n"
+    "-alg  --alg           name:s                                          \n"
+    "-s    --seed          value:i                                         \n"
+    "-pt   --printtree                                                     \n"
+    "-val  --validate                                                      \n"
+    "-nd   --nodelaunay                                                    \n"
+    "-st   --stats                                                         \n"
+    "-out  --outfile       path:s                                          \n"
+    "-lt   --looptime      sec:i                                           \n"
+    "-b    --batch                                                         \n"
+    "-g    --generate      set_type:s dimension:i num_points:i             \n"
+    "-gn   --generateN     set_type:s dimension:i num_points:i num_tests:i \n"
+    "-if   --infile        file:s set_name:s                               \n"
+    "-ifa  --infileall     file:s                                          \n"
+    "PARAMETERS:                                                           \n"
+    "                                                                      \n"
+    "file:s set_name:s                                                     \n"
+    "set_type:s dimension:i num_points:i                                   \n";
+    
+  CLAP c(clap_config, argc, argv);
+  
+  if(c.is_set("testtopo")) {
     std::cout << "Running topo test!" << std::endl;
     std::string name("Smith iterative");
     std::string resFile("/home/stephan/Desktop/result.dat");
@@ -45,47 +81,32 @@ int main(int argc, char *argv[]) {
     path = "../data/protein3D/W3.stp";
     sname = "W3";
     std::vector<Point> points2 = Generator::loadFromFile(path, sname);
-
+    
     ESMT e1(points1);
     ESMT e2(points2);
-
+    
     Test test;
     std::cout << test.testTopology(e1,e2) << std::endl;
-
+    
     return 0;
   }
-  else if(std::string("counttest").compare(0, 20, argv[1]) == 0) {
-    if(argc != 5)
-      usage();
-    int d = atoi(argv[2]), n = atoi(argv[3]), seed = atoi(argv[4]);
+  else if(c.is_set("counttest")) {
+    int d = c.get_int_param("counttest",0),
+      n = c.get_int_param("counttest",1),
+      seed = c.get_int_param("counttest",2);
     Test test;
     test.doTestESMTSpecial(d, n, seed);
     return 0;
   }
   else {
-    /*
-     * See usage() below for options
-     */
-    std::string v("-v");
-    std::string pt("-pt");
-    std::string npo("-npo");
-    std::string nsc("-nsc");
-    std::string nd("-nd");
-    std::string cs("-cs");
-    std::string out("-out");
-    std::string lt("-lt");
-    std::string alg("-alg");
-    std::string s("-s");
-    std::string sct("-sct");
-    std::string val("-val");
-    bool verbose = getBool(v, argc, argv);
-    bool print   = getBool(pt, argc, argv);
-    int n, t, tests, dim;
-    std::string algname, outfile, filename, setname;
+    bool verbose = c.is_set("v");
+    bool print   = c.is_set("pt");
+    int n, t = -1, dim, numtests;
+    std::string algname, outfile, filename, settype, setname;
     SubgraphHeuristic *sh;
     SubgraphHeuristic *shsp = NULL;
-    algname = getString(alg, argc, argv);
-    if(algname.size()==0 || algname.compare(0, 10, "NO") == 0)
+    algname = c.is_set("alg") ? c.get_string_param("alg") : "NO";
+    if(algname.compare(0, 10, "NO") == 0)
       sh = new IterativeSmith(MAX_DIM);
     else if(algname.compare(0, 10, "RNO") == 0)
       sh = new IterativeConcat(MAX_DIM);
@@ -94,110 +115,115 @@ int main(int argc, char *argv[]) {
       sh = new SteinerFinder(shsp);
     }
     else {
-      std::cerr << "Unknown sub-graph algorithm: " << algname << std::endl;
-      usage();
+      c.error_usage("Unknown sub-graph algorithm");
+      return 1;
     }
-    if(std::string("test").compare(0, 20, argv[1]) == 0) {
-      if(argc < 3)
-	usage();
+
+    // Heuristic parameters
+    bool no_subgraph_concat = c.is_set("nsc"),
+      no_post_optimise = c.is_set("npo"),
+      redo_concat = c.is_set("rdc");
+    int seed = c.is_set("s") ? c.get_int_param("s") : time(NULL);
+    
+    int p = c.get_chosen_pattern();
+    if(p == 0 && !c.is_set("batch")) {
+      delete sh;
+      delete shsp;
+      c.error_usage("Please use --batch when no inputs given.");
+    }
+    else if(p == 0) {      
+      // Check that some set is added
+      if(!c.is_set("g") && !c.is_set("gn") && !c.is_set("if") && !c.is_set("ifa")) {
+	delete sh;
+	delete shsp;
+	c.error_usage("No input set specified");
+      }
+    
       Test test;
       test.setSubgraphHeuristicOne(sh, algname);
-      test.doConcatSubgraphs(!getBool(nsc, argc, argv));
-      test.doPostOptimise(!getBool(npo, argc, argv));
-      test.doUseSpecialConcat(getBool(sct, argc, argv));
-      test.inclDelaunay(!getBool(nd, argc, argv));
-      test.setLoopTime(getInt(lt, argc, argv, 0));
-      test.setSeed(getInt(s, argc, argv, time(NULL)));
-      test.doCollectStats(getBool(cs, argc, argv));
-      outfile = getString(out, argc, argv);
-      // Add tests
-      bool tests_ok = false;
-      for(int i = 2; i < argc; i++) {
-	if(std::string("-in").compare(0, 20, argv[i]) == 0) {
-	  if(i >= argc-2) {
-	    std::cerr << "Missing arguments for -in" << std::endl;
-	    usage();
-	  }
-	  filename = argv[i+1];
-	  setname = argv[i+2];
-	  test.addTest(filename,setname);
-	  tests_ok = true;
-	}
-	else if(std::string("-ina").compare(0, 20, argv[i]) == 0) {
-	  if(i >= argc-1) {
-	    std::cerr << "Missing arguments for -ina" << std::endl;
-	    usage();
-	  }
-	  filename = argv[i+1];
-	  test.addTest(filename);
-	  tests_ok = true;
-	}
-	else if(std::string("-g").compare(0, 20, argv[i]) == 0
-		|| std::string("-gn").compare(0, 20, argv[i]) == 0) {
-	  if(i >= argc-3) {
-	    std::cerr << "Missing arguments for -g" << std::endl;
-	    usage();
-	  }
-	  setname = argv[i+1];
-	  dim     = atoi(argv[i+2]);
-	  n       = atoi(argv[i+3]);
-	  if(setname.compare(0, 20, "random") == 0)
-	    t = TEST_POINT_SET_RANDOM_D;
-	  else if(setname.compare(0, 20, "simplex") == 0)
-	    t = TEST_POINT_SET_SIMPLEX;
-	  else if(setname.compare(0, 20, "sausage") == 0)
-	    t = TEST_POINT_SET_SAUSAGE;
-	  else if(setname.compare(0, 20, "grid") == 0)
-	    t = TEST_POINT_SET_GRID;
-	  else if(setname.compare(0, 20, "delaunay") == 0)
-	    t = TEST_POINT_SET_DELAUNAY_SIMPLICES;
-	  else {
-	    std::cerr << "Unknown point set used with "
-		      << argv[i] << ": " << setname << std::endl;
-	    usage();
-	  }
-	  tests = 1;
-	  if(std::string("-gn").compare(0, 20, argv[i]) == 0) {
-	    if(i >= argc-4) {
-	      std::cerr << "Missing arguments for -gn" << std::endl;
-	      usage();
-	    }
-	    tests = atoi(argv[i+4]);
-	  }
-	  test.addTest(t, dim, n, tests);
-	  tests_ok = true;
-	}
+      test.doConcatSubgraphs(!no_subgraph_concat);
+      test.doPostOptimise(!no_post_optimise);
+      test.doUseSpecialConcat(redo_concat);
+      test.inclDelaunay(!c.is_set("nd"));
+      test.setLoopTime(c.is_set("lt") ? c.get_int_param("lt") : 0);
+      test.setSeed(seed);
+      test.doCollectStats(c.is_set("st"));
+      outfile = c.is_set("out") ? c.get_string_param("out") : "";
+      
+      unsigned int i, num_g = c.is_set("g"), num_gn = c.is_set("gn"),
+	num_if = c.is_set("if"), num_ifa = c.is_set("ifa");
+      
+      numtests = 1;
+      for(i = 0; i < num_g; i++) {
+	settype  = c.get_string_param("g", 0, i);
+	dim      = c.get_int_param("g", 1, i);
+	n        = c.get_int_param("g", 2, i);
+	t        = set_type_from_name(settype);
+	if(t < 0)
+	  c.error_usage("Unknown point set type");
+	test.addTest(t, dim, n, numtests);
       }
-      if(!tests_ok) {
-	std::cerr << "You must include at least one point set for testing (use -in, -ina, -g or -gn)." << std::endl;
-	usage();
+      for(i = 0; i < num_gn; i++) {
+	settype  = c.get_string_param("gn", 0, i);
+	dim      = c.get_int_param("gn", 1, i);
+	n        = c.get_int_param("gn", 2, i);
+	numtests = c.get_int_param("gn", 3, i);
+	t        = set_type_from_name(settype);
+	if(t < 0)
+	  c.error_usage("Unknown point set type");
+	test.addTest(t, dim, n, numtests);
       }
-      if(std::string("esmt").compare(0, 20, argv[2]) == 0)
-	test.doTestESMT(verbose);
-      else if(std::string("sgh").compare(0, 20, argv[2]) == 0)
+      for(i = 0; i < num_if; i++) {
+	filename = c.get_string_param("if", 0, i);
+	setname  = c.get_string_param("if", 1, i);
+	test.addTest(filename, setname);
+      }
+      for(i = 0; i < num_ifa; i++) {
+	filename = c.get_string_param("ifa", 0, i);
+	test.addTest(filename);
+      }
+      
+      if(c.is_set("os"))
 	test.doTestSubgraphAlgorithm(verbose);
       else
-	usage();
+	test.doTestESMT(verbose);
       if(outfile.size() > 0)
 	test.createDatFile(outfile);
     }
-    else if(std::string("esmt").compare(0, 20, argv[1]) == 0) {
-      Generator::setSeed(getInt(s, argc, argv, time(NULL)));
-      std::vector<Point> points = getSet(argc, argv);
-      if(points.size() == 0) {
-	std::cerr << "Empty or no point set given!" << std::endl;
-	usage();
+    else {
+      std::vector<Point> points;
+      if(p == 1) {
+	filename = c.get_string_param("file");
+	setname  = c.get_string_param("set_name");
+	points = Generator::loadFromFile(filename,setname);
       }
-      ESMT esmt(points, sh, !getBool(nsc, argc, argv),
-		!getBool(npo, argc, argv), getBool(sct, argc, argv), verbose);
+      else if(p == 2) {
+	settype = c.get_string_param("set_type");
+	dim     = c.get_int_param("dimension");
+	n       = c.get_int_param("num_points");
+	Generator::setSeed(seed);
+	if(settype.compare(0, 20, "random") == 0)
+	  points = Generator::randomFloatPoints(Point(dim, 100), Point(dim, -100), n);
+	else if(settype.compare(0, 20, "simplex") == 0)
+	  points = Generator::simplex(dim);
+	else if(settype.compare(0, 20, "sausage") == 0)
+	  points = Generator::sausage(dim, n);
+	else if(settype.compare(0, 20, "grid") == 0)
+	  points = Generator::gridFromSide(dim, n);
+	else
+	  c.error_usage("Unknown point set type");
+      }
       
-      if(getBool(val, argc, argv)) {
+      ESMT esmt(points, sh, !no_subgraph_concat,
+		!no_post_optimise, redo_concat, verbose);
+      
+      if(c.is_set("val")) {
 	if(Utils::validate(esmt))
 	  std::cout << "Validate OK!" << std::endl;
 	else
 	  std::cout << "Validate ERROR!" << std::endl;
       }
-
+      
       if(print)
 	std::cout << "## RESULT ##" << std::endl << esmt << std::endl;
       else {
@@ -207,18 +233,16 @@ int main(int argc, char *argv[]) {
 		  << "  Ratio = " << esmt.getSteinerRatio() << std::endl;
       }
     }
-    else if(std::string("sgh").compare(0, 20, argv[1]) == 0) {
-      std::cerr << "Not implemented yet." << std::endl;
-      exit(1);
-    }
+    
     delete sh;
     delete shsp;
+    
     return 0;
   }
-  std::cout << "argument fell through, functionality not yet implemented!" << std::endl;
-  return 1;
 }
 
+
+/*
 void usage() {
   std::cout << "Usage:" << std::endl
 	    << " esmt [options] <points>" << std::endl
@@ -263,80 +287,4 @@ void usage() {
 	    << "                                 dimension."<<std::endl;
   exit(1);
 }
-
-std::string getString(std::string tag, int argc, char *argv[], bool mandatory) {
-  for(int i = 2; i < argc; i++) {
-    if(tag.compare(0, 20, argv[i]) == 0) {
-      if(i == argc-1) {
-	std::cerr << "Missing argument for " << tag << std::endl;
-	usage();
-      }
-      return std::string(argv[i+1]);
-    }
-  }
-  if(mandatory)  {
-    std::cerr << "Missing argument " << tag << std::endl;
-    usage();
-  }
-  return "";
-}
-
-bool getBool(std::string tag, int argc, char *argv[]) {
-  for(int i = 2; i < argc; i++) {
-    if(tag.compare(0, 20, argv[i]) == 0)
-      return true;
-  }
-  return false;
-}
-
-int getInt(std::string tag, int argc, char *argv[], int def, bool mandatory) {
-  for(int i = 2; i < argc; i++) {
-    if(tag.compare(0, 20, argv[i]) == 0) {
-      if(i == argc-1) {
-	std::cerr << "Missing argument for " << tag << std::endl;
-	usage();
-      }
-      return atoi(argv[i+1]);
-    }
-  }
-  if(mandatory)  {
-    std::cerr << "Missing argument " << tag << std::endl;
-    usage();
-  }
-  return def;
-}
-
-std::vector<Point> getSet(int argc, char *argv[]) {
-  std::vector<Point> result;
-  std::string file, name;
-  int dim, n;
-  for(int i = 2; i < argc; i++) {
-    if(std::string("-in").compare(0, 20, argv[i]) == 0) {
-      if(i >= argc-2) {
-	std::cerr << "Missing arguments for -in" << std::endl;
-	usage();
-      }
-      file = argv[i+1];
-      name = argv[i+2];
-      result = Generator::loadFromFile(file,name);
-    }
-    else if(std::string("-g").compare(0, 20, argv[i]) == 0) {
-      if(i >= argc-3) {
-	std::cerr << "Missing arguments for -g" << std::endl;
-	usage();
-      }
-      name = argv[i+1];
-      dim  = atoi(argv[i+2]);
-      n    = atoi(argv[i+3]);
-      if(name.compare(0, 20, "random") == 0)
-	result  = Generator::randomFloatPoints(Point(dim, 100), Point(dim, -100), n);
-      else if(name.compare(0, 20, "simplex") == 0)
-	result  = Generator::simplex(dim);
-      else if(name.compare(0, 20, "sausage") == 0)
-	result  = Generator::sausage(dim, n);
-      else if(name.compare(0, 20, "grid") == 0)
-	result  = Generator::gridFromSide(dim, n);
-    }
-  }
-  return result;
-}
+*/
